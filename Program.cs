034b57af3a78c2b38dec58dd20a3f1fd696ec668
PhilipBrydeProgrammer <brydephilip@gmail.com>
2025-09-26 11:15:47 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -84,7 +84,14 @@ namespace cody_practice_v2
             Console.WriteLine("Ny Øvelse");
             Console.ResetColor();
 
-            // Du kan tilføje mere kode her senere
+            int a = 5;
+            a += 3; 
+            Console.WriteLine(a);
+
+            int b = 100;
+            b-= 10; 
+            Console.WriteLine(b);
+
         }
 
         static void incrementDecrement()
@@ -95,7 +102,22 @@ namespace cody_practice_v2
             a--;
             
             Console.WriteLine(a);
+          //short cut til increment/decrement 
+            int b = 10;
+            b += 10; 
+            Console.WriteLine(b);
             
+            //bool true false greater or equal to coody example 
+            int c = 10;
+            int d = 10;
+            bool result = c <= d;
+                Console.WriteLine(c <=d);
+            //coody lesson bool to see if n1 is greater then n2     
+            var n1 = 8;
+            var n2 = 9; 
+            bool n3 = n1>n2; 
+            Console.WriteLine(n3);
+
         }
     }
 }
