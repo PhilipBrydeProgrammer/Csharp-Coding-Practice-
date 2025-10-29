2a5d3e12d8dc340cf8b491d141dac574feb76266
bryde <bryde@FONMU-3HR8G5O72>
2025-10-29 21:32:36 +0100

    Add project files.

---
+++ SLETMIG_1/Program.cs
@@ -0,0 +1,35 @@
+﻿//Simpel debug af array og funktion
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = new int[] { 4, 6, 2, 33, 5 };
+        int svar = A(arr);
+        Console.WriteLine(svar);
+        int A(int[] arr)
+        {
+            int svar = 0;
+            int pos = arr.Length - 1;
+            svar = arr[pos];
+            return svar;
+        }
+
+        Simple person = new Simple();
+        person.ShowInfo();
+    }
+}
+
+ class Simple
+ { 
+    
+    int age = 23;
+    string name = "Philip Bryde";
+    string occupation = "*Athlete*";
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"{name} {age} {occupation}");  
+    }
+
+    
+ }
